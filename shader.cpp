e04ea27f758664be8c86fc9ae7064483c06b9047
aoowweenn <aoowweenn@gmail.com>
2017-05-19 16:13:57 +0800

    try OO

--- shader.cpp
+++ shader.cpp
@@ -1,15 +1,97 @@
 #include "shader.h"
 #include <iostream>
 #include <fstream>
+#include <sstream>
+#include <string>
 using namespace std;
+
+Shader::Shader(const GLchar* vertexPath, const GLchar* fragmentPath)
+{
+    string vertex_code, fragment_code;
+    vertex_code = loadFile(vertexPath);
+    fragment_code = loadFile(fragmentPath);
+    const GLchar* vShaderCode = vertex_code.c_str();
+    const GLchar* fShaderCode = fragment_code.c_str();
+    GLuint vShader = compile(vShaderCode, GL_VERTEX_SHADER);
+    GLuint fShader = compile(fShaderCode, GL_FRAGMENT_SHADER);
+    this->program = link(vShader, fShader);
+}
+
+void Shader::use()
+{
+    glUseProgram(this->program);
+}
+
+GLuint Shader::link(const GLuint vShader, const GLuint fShader)
+{
+    GLuint program = glCreateProgram();
+    glAttachShader(program, vShader);
+    glAttachShader(program, fShader);
+    glLinkProgram(program);
+
+    GLint success;
+    GLchar infoLog[512];
+
+    glGetProgramiv(program, GL_LINK_STATUS, &success);
+    if(!success)
+    {
+        glGetProgramInfoLog(program, 512, NULL, infoLog);
+        std::cerr << "[ERROR] Linking failed\n" << infoLog << std::endl;
+        std::exit(-1);
+    }
+
+    glDeleteShader(vShader);
+    glDeleteShader(fShader);
+    return program;
+}
+
+GLuint Shader::compile(const GLchar* code, GLenum shaderType)
+{
+    GLuint shader = glCreateShader(shaderType);
+    glShaderSource(shader, 1, (const GLchar**)&code, NULL);
+    glCompileShader(shader);
+
+    GLint success;
+    GLchar infoLog[512];
+
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+
+    if(!success)
+    {
+        glGetShaderInfoLog(shader, 512, NULL, infoLog);
+        std::cerr << "[ERROR] Compilation failed\n" << infoLog << std::endl;
+        std::exit(-1);
+    }
+    return shader;
+}
+
+std::string Shader::loadFile(const GLchar* filename)
+{
+    ifstream fs;
+    fs.exceptions(ifstream::failbit | ifstream::badbit);
+    try
+    {
+        fs.open(std::string("shader/") + filename);
+        std::stringstream ss;
+        ss << fs.rdbuf();
+        fs.close();
+        return ss.str();
+    }
+    catch(ifstream::failure e)
+    {
+        std::cerr << "[ERROR] " << e.what() << std::endl;
+        std::exit(-1);
+    }
+    return std::string("");
+}
 
 const char* loadCStringFromFile(string filename)
 {
     ifstream ifs(filename.c_str(), fstream::in | fstream::ate);
     if(!ifs.is_open())
     {
-        cout << "Can't load file: " << filename << endl;
-        exit(-1);
+        std::cout << "Can't load file: " << filename << std::endl;
+        std::exit(-1);
     }
 
     fstream::pos_type fileSize = ifs.tellg();
@@ -38,7 +120,7 @@ GLuint loadShader(GLenum shaderType, std::string filename)
     if(!success)
     {
         glGetShaderInfoLog(shader, 512, NULL, infoLog);
-        cout << "[ERROR] Compilation failed: " << filename << endl;
+        std::cout << "[ERROR] Compilation failed: " << filename << std::endl;
     }
 
     delete[] prog;
--- shader.h
+++ shader.h
@@ -1,4 +1,20 @@
+#ifndef SHADER_H
+#define SHADER_H
+
 #include <GL/glew.h>
 #include <string>
 
+class Shader
+{
+public:
+    GLuint program;
+    Shader(const GLchar* vertexPath, const GLchar* fragmentPath);
+    void use();
+    std::string loadFile(const GLchar* path);
+    GLuint compile(const GLchar* code, GLenum shaderType);
+    GLuint link(const GLuint vShader, const GLuint fShader);
+};
+
 GLuint loadShader(GLenum shaderType, std::string filename);
+
+#endif
